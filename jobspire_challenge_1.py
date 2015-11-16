7086465aadc55a242c6b1fc3af89c6b3e2081ddb
manty123 <deepaknayak500@gmail.com>
2015-11-16 20:37:26 +0530

    Update jobspire_challenge_1.py

--- jobspire_challenge_1.py
+++ jobspire_challenge_1.py
@@ -3,7 +3,7 @@ import random
 def print_name_number(name):
     #Generate a random number between 6 to 15
     rand_number = random.randrange(6, 15)
-	#Print the output
+    #Print the output
     print(name + ' ' + str(rand_number))
 
 #Take input from user 
